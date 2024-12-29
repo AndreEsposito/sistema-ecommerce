9bed8c2746aa2ce8f9681bad703519331b5389a1
André <andrezinhoesposito33@gmail.com>
2024-12-29 12:13:31 -0300

    first commit

---
+++ src/main/java/br/com/sistema_ecommerce/SistemaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package br.com.sistema_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sistema_ecommerce/SistemaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistema_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
