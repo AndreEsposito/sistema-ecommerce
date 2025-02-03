3fe5464385565eeee2023f963073e2b70378a712
André <andrezinhoesposito33@gmail.com>
2025-02-02 22:19:46 -0300

    ajustando scripts sql

--- src/main/java/br/com/sistema_ecommerce/config/database/scripts.sql
+++ src/main/java/br/com/sistema_ecommerce/config/database/scripts.sql
@@ -14,9 +14,16 @@ CREATE TABLE produto (
 
 CREATE TABLE produto_eletronico (
     id INT PRIMARY KEY,
-    marca VARCHAR(255) NOT NULL,
-    modelo VARCHAR(255) NOT NULL,
-    duracao_bateria VARCHAR(255),
+    marca VARCHAR(50) NOT NULL,
+    modelo VARCHAR(50) NOT NULL,
+    duracao_bateria VARCHAR(50),
     peso DECIMAL(10, 2),
+    FOREIGN KEY (id) REFERENCES produto (id)
+);
+
+CREATE TABLE produto_roupa (
+    id INT PRIMARY KEY,
+    tamanho VARCHAR(10) NOT NULL,
+    materia VARCHAR(50) NOT NULL,
     FOREIGN KEY (id) REFERENCES produto (id)
 );
